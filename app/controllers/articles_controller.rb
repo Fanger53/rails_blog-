8d545695d8148f546cd558ea4134cc4eb6ef7997
David Leonardo <fanger53@gmail.com>
2020-11-24 12:45:30 -0500

    add new and destroy method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,28 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
     def index
         @articles = Article.all
       end
     
     def show
       @article = Article.find(params[:id])
+    end
+
+    def new
+      @article = Article.new
+    end
+
+    def create
+      @article = Article.new(article_params)
+      @article.save
+      redirect_to article_path(@article)
+    
+    end
+    
+    def destroy
+      @article = Article.destroy(params[:id])
+      @article.save
+      redirect_to article_path(@article)
     end
       
 end
