3a2d1050fb049190e453a6f6c4bd56edcf6f4a72
David Leonardo <fanger53@gmail.com>
2020-11-24 10:21:29 -0500

    add show action and style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,10 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
       end
+    
+    def show
+      @article = Article.find(params[:id])
+
+    end
       
 end
