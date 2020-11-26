317b6b823ed131aefc4d8fffce2ed6cec84dd20c
David Leonardo <fanger53@gmail.com>
2020-11-26 10:11:03 -0500

    add paperclipgem

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,7 @@ class Article < ActiveRecord::Base
     has_many :tags, through: :taggings
     has_attached_file :image
     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 
 
     def tag_list
